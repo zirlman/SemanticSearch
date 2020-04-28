f0a9c333d721b5bb070572c3f3d785f063088873
zirlman <dragec.96@live.com>
2020-04-28 10:00:01 +0200

    Change search bar style; Add keyup listener & click listener

--- search.js
+++ search.js
@@ -1,45 +1,31 @@
-// The user clicked the button!
 function sendMsg(tabs) {
-  // 'tab' is an object with information about the current open tab
   let tab = tabs[0];
   let msg = {
-    message: "user clicked!",
+    content: "user clicked!",
   };
 
   chrome.tabs.sendMessage(tab.id, msg);
   console.log("Message sent");
 }
 
+function keyUp(event) {
+  if (event.keyCode == 13) {
+    setContent(event);
+  }
+}
+
+function setContent(event) {
+  event.preventDefault();
+  document.querySelector("#msg").innerHTML = document.querySelector(
+    "#searchInput"
+  ).value;
+}
+
 function onLoad() {
-  const input = document.querySelector(".finder__input");
-  const finder = document.querySelector(".finder");
-  const form = document.querySelector("form");
+  document.querySelector("#searchButton").addEventListener("click", setContent);
+  document.querySelector("#searchInput").addEventListener("keyup", keyUp);
 
-  input.addEventListener("focus", () => {
-    finder.classList.add("active");
-  });
-
-  input.addEventListener("blur", () => {
-    if (input.value.length === 0) {
-      finder.classList.remove("active");
-    }
-  });
-
-  form.addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    finder.classList.add("processing");
-    finder.classList.remove("active");
-    input.disabled = true;
-    msg.innerHTML = input.value;
-    setTimeout(() => {
-      finder.classList.remove("processing");
-      input.disabled = false;
-      if (input.value.length > 0) {
-        finder.classList.add("active");
-      }
-    }, 1000);
-  });
-
+  // Query active tab
   let params = { active: true, currentWindow: true };
   chrome.tabs.query(params, sendMsg);
 }
