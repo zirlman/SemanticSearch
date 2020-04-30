7fe49ab3aeff2baf8f161aa12b747cbab617e73c
zirlman <dragec.96@live.com>
2020-04-30 16:25:31 +0200

    Add axios support; Add ngrock

--- src/search.js
+++ src/search.js
@@ -30,28 +30,58 @@ function makeCard(text) {
   return card;
 }
 
-function setContent(event) {
-  event.preventDefault();
-  let question = document.querySelector("#searchInput").value;
-  document.querySelector("#msg").innerHTML = question;
-
-  // TODO: Get paragraphs from content.js
-  // TODO: Format model input to "question: quest context: ...."
-  // TODO: Return generated answers to this array
-  let bgpage = chrome.extension.getBackgroundPage();
-  paragraphs = bgpage.paragraphs;
-
+function updateAnswers(question, answers) {
   let divAnswers = document.querySelector("#answers");
   // Remove old answers
   while (divAnswers.firstChild) {
     divAnswers.removeChild(divAnswers.firstChild);
   }
-  console.log(paragraphs);
-  // Add new answers
-  for (let i = 0; i < paragraphs.length; i++) {
-    let card = makeCard(question + " " + paragraphs[i]);
+
+  // Make answers
+  for (let i = 0; i < answers.length; i++) {
+    let card = makeCard("- " + answers[i]);
     divAnswers.appendChild(card);
   }
+}
+
+const axios = require("axios");
+
+function getAnswers(input, question) {
+  console.log(input);
+  let data = { input };
+  let url = "http://6414f116.ngrok.io/api/t5_model";
+
+  axios
+    .post(url, data)
+    .then((response) => {
+      console.log("IN PROMISE " + response);
+      updateAnswers(question, response.data.answers);
+    })
+    .catch((error) => {
+      console.log(error);
+      updateAnswers(question, []);
+    });
+}
+
+function setContent(event) {
+  event.preventDefault();
+  let question = document.querySelector("#searchInput").value;
+  document.querySelector("#msg").innerHTML = question;
+
+  let bgpage = chrome.extension.getBackgroundPage();
+  paragraphs = bgpage.paragraphs;
+
+  // Make model input
+  let input = "question: " + question + " context: ";
+  let context = "";
+  let maxContextLen = 512 - input.length;
+  for (let i = 0; i < paragraphs.length && context.length < maxContextLen; i++)
+    context += paragraphs[i];
+  input += context;
+
+  // Get model output
+  let answers = getAnswers(input, question);
+  console.log("RESPONSE: " + answers);
 }
 
 function onLoad() {
@@ -67,23 +97,19 @@ document.addEventListener("DOMContentLoaded", onLoad);
 
 // -================== TENSORFLOW ==================-
 
-import * as tf from "@tensorflow/tfjs";
+// import * as tf from "@tensorflow/tfjs";
+// const tokenizer_path =
+//   "https://s3.amazonaws.com/models.huggingface.co/bert/t5-spiece.model";
+// // const model_path = "../libraries/models/t5_base_model_js/model.json";
+// const model_path = "../libraries/models/t5-small-tf_model/model.json";
+// // const model_path = "https://cdn.huggingface.co/t5-small-tf_model.h5";
 
-// Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-
-model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
-
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-// Train the model using the data.
-model.fit(xs, ys, { epochs: 10 }).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
-  // Open the browser devtools to see the output
-});
+// document.querySelector("#msg").innerText = "[LOADING TF MODEL]";
+// var model;
+// tf.loadLayersModel(model_path).then((loadedModel) => {
+//   model = loadedModel;
+//   document.querySelector("#msg").innerText = "[MODEL LOADED]";
+// });
+// console.log("After promise");
 
 // -================================================-
