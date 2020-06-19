62ae289b8e6e21b469163b28e6d6827a7958d6a4
zrilic <extern.dragan.zrilic@tttech-auto.com>
2020-06-19 11:04:39 +0200

    Update host IP

--- ChromeExtension/src/search.js
+++ ChromeExtension/src/search.js
@@ -56,7 +56,8 @@ function getAnswers(input) {
   const ID = "cb622656";
   // const URL = `http://${ID}.ngrok.io/api/qa`;
   // const URL = `http://localhost:5000/api/qa`;
-  const URL = "http://34.105.205.14:5000/api/qa"
+  const URL = "http://35.246.116.206:5000/api/qa";
+
   const data = { input };
 
   axios
@@ -116,9 +117,6 @@ document.addEventListener("DOMContentLoaded", onLoad);
 // const context = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet."
 // const question = "Who is the CEO of Google?"
 
-
-
-
 // async function test() {
 //   // Load the model.
 //   const model = await qna.load();
@@ -135,7 +133,5 @@ document.addEventListener("DOMContentLoaded", onLoad);
 //   score: 0.8380282521247864
 // }]
 // **/
-
-
 
 // -================================================-
